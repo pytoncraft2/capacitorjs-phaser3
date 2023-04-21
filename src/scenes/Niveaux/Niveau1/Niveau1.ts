0931062000e6b1e9da38893dbf16f53ece6598e4
pytoncraft2 <pythoncraft2@gmail.com>
2023-04-21 14:19:00 +0200

    🐛 2nd collider bug ?

--- src/scenes/Niveaux/Niveau1/Niveau1.ts
+++ src/scenes/Niveaux/Niveau1/Niveau1.ts
@@ -6,6 +6,8 @@
 import BaseNiveaux from "../BaseNiveaux";
 import OnPointerDownScript from "../../../script-nodes-basic/OnPointerDownScript";
 import StartSceneActionScript from "../../../script-nodes-basic/StartSceneActionScript";
+import Entite from "../../Entites/Entite";
+import PlatformePrefab from "../ObjetsNiveaux/PlatformePrefab";
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
 
@@ -22,7 +24,7 @@ export default class Niveau1 extends BaseNiveaux {
 	editorCreate(): void {
 
 		// text_1
-		const text_1 = this.add.text(865, 469, "", {});
+		const text_1 = this.add.text(1574, 63, "", {});
 		text_1.text = "New text";
 		text_1.setStyle({ "fontSize": "56px" });
 
@@ -31,6 +33,35 @@ export default class Niveau1 extends BaseNiveaux {
 
 		// startSceneActionScript
 		const startSceneActionScript = new StartSceneActionScript(onPointerDownScript);
+
+		// players
+		const players = this.add.layer();
+
+		// entite
+		const entite = new Entite(this, 449, 153);
+		players.add(entite);
+
+		// entite_1
+		const entite_1 = new Entite(this, 1208, 346);
+		players.add(entite_1);
+
+		// platformes
+		const platformes = this.add.layer();
+
+		// platformePrefab
+		const platformePrefab = new PlatformePrefab(this, 1780, 663);
+		platformePrefab.scaleX = 12.955064222093448;
+		platformePrefab.scaleY = 1;
+		platformes.add(platformePrefab);
+
+		// platformePrefab_1
+		const platformePrefab_1 = new PlatformePrefab(this, -2, 476);
+		platformePrefab_1.scaleX = 12.955064222093448;
+		platformePrefab_1.scaleY = 1;
+		platformes.add(platformePrefab_1);
+
+		// collider
+		this.physics.add.collider(players.list, platformes.list);
 
 		// startSceneActionScript (prefab fields)
 		startSceneActionScript.sceneKey = "Niveau2";
@@ -45,8 +76,8 @@ export default class Niveau1 extends BaseNiveaux {
 	create() {
 
 		// this.editorCreateBase()
-		// this.editorCreate();
-		console.log("NIVEAU 1!!!!!!!!!!!!!!!!!!!!");
+		this.editorCreate();
+		console.log("NIVEAU 1!!!!!!!!!!!!!!!!!!!!", this);
 
 		// this.allies.add(this.entite)
 		// console.log(this.entites_vs_platformes);
