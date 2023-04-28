2a43816e478bde3b7e1221b832eada6d49c0d6ef
pytoncraft2 <pythoncraft2@gmail.com>
2023-04-28 21:15:23 +0200

    🧪 test muliple case

--- src/scenes/Entites/Entite.ts
+++ src/scenes/Entites/Entite.ts
@@ -45,6 +45,7 @@ export default class Entite extends Phaser.GameObjects.Container {
 	public velociteY: number = 890;
 	public tempsCumule: number = 0;
 	public tempsEntreActions: number = 1000;
+	public bonusVelociteX: number = 0;
 
 	/* START-USER-CODE */
 	public Aptitudes: Touches = { ...Aptitudes }
