bfb91f57e055e68fb65ab8848d4e6959cc261e33
pytoncraft2 <pythoncraft2@gmail.com>
2023-05-01 18:26:15 +0200

    📝 documentation fonction flipX et zone interaction

--- src/scenes/Entites/Aptitudes/Defaut/deplacements.ts
+++ src/scenes/Entites/Aptitudes/Defaut/deplacements.ts
@@ -1,42 +1,54 @@
 import Entite from "../../Entite"
 
-export function saut(joueur: Entite) {
-  joueur.body.checkCollision.none = true;
+export function saut(entite: Entite) {
+  entite.body.checkCollision.none = true;
   //TODO CHANGER VITESSE DEPLACMENT EN Y
-  joueur.body.setVelocityY(-joueur.velociteY);
-  joueur.scene.time.delayedCall(500, () => (joueur.body.checkCollision.none = false), undefined, joueur);
+  entite.body.setVelocityY(-entite.velociteY);
+  entite.scene.time.delayedCall(500, () => (entite.body.checkCollision.none = false), undefined, entite);
 }
-export function stopEntite(joueur: Entite) {
-  joueur.body.setVelocityX(0);
+export function stopEntite(entite: Entite) {
+  entite.body.setVelocityX(0);
 }
-export function deplacementDroite(joueur: Entite) {
-  joueur.body.setVelocityX(joueur.velociteX);
+export function deplacementDroite(entite: Entite) {
+  entite.body.setVelocityX(entite.velociteX);
 
-  FlipX(joueur, false)
-  ZoneInteractionADroite(joueur)
+  FlipX(entite, false)
+  ZoneInteractionADroite(entite)
 }
-export function deplacementGauche(joueur: Entite) {
-  joueur.body.setVelocityX(-joueur.velociteX)
+export function deplacementGauche(entite: Entite) {
+  entite.body.setVelocityX(-entite.velociteX)
 
-  FlipX(joueur, true)
-  ZoneInteractionAGauche(joueur)
+  FlipX(entite, true)
+  ZoneInteractionAGauche(entite)
 }
-export function deplacementBas(joueur: Entite): any {
-  joueur.body.checkCollision.none = true;
-  joueur.body.setVelocityY(joueur.velociteY)
-  joueur.scene.time.delayedCall(50, () => (joueur.body.checkCollision.none = false), undefined, joueur);  // delay in ms
-}
-
-export function ZoneInteractionADroite(joueur: Entite) {
-  if (joueur.rectangle_zone_interaction.x != joueur.image_entite.getRightCenter().x)
-    joueur.rectangle_zone_interaction.setPosition(joueur.image_entite.getRightCenter().x, joueur.image_entite.getRightCenter().y);  
+export function deplacementBas(entite: Entite): any {
+  entite.body.checkCollision.none = true;
+  entite.body.setVelocityY(entite.velociteY)
+  entite.scene.time.delayedCall(50, () => (entite.body.checkCollision.none = false), undefined, entite);  // delay in ms
 }
 
-export function ZoneInteractionAGauche(joueur: Entite) {
-  if (joueur.rectangle_zone_interaction.x != joueur.image_entite.getLeftCenter().x)
-    joueur.rectangle_zone_interaction.setPosition(joueur.image_entite.getLeftCenter().x, joueur.image_entite.getLeftCenter().y);  
+/**
+ * @param entite
+ * @description déplace à droite de l'image, le rectangle qui permet de détecter une autre entité 
+ */
+export function ZoneInteractionADroite(entite: Entite) {
+  if (entite.rectangle_zone_interaction.x != entite.image_entite.getRightCenter().x)
+    entite.rectangle_zone_interaction.setPosition(entite.image_entite.getRightCenter().x, entite.image_entite.getRightCenter().y);  
 }
 
-export function FlipX(joueur: Entite, ouiNon: boolean) {
-  joueur.image_entite.setFlipX(ouiNon);
+/**
+ * @param entite
+ * @description déplace à gauche de l'image, le rectangle qui permet de détecter une autre entité 
+ */ 
+export function ZoneInteractionAGauche(entite: Entite) {
+  if (entite.rectangle_zone_interaction.x != entite.image_entite.getLeftCenter().x)
+    entite.rectangle_zone_interaction.setPosition(entite.image_entite.getLeftCenter().x, entite.image_entite.getLeftCenter().y);  
+}
+
+/**
+ * @param entite 
+ * @param ouiNon retourne l'image de l'entité vers la droite(false) ou vers la gauche(true)
+ */
+export function FlipX(entite: Entite, ouiNon: boolean) {
+  entite.image_entite.setFlipX(ouiNon);
 }
