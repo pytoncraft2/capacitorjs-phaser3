a4264c4de09ee20c1636626f26e6c739fceee31c
pytoncraft2 <pythoncraft2@gmail.com>
2023-05-02 21:15:56 +0200

    🚧 envoie projectile toile test

--- src/scenes/Entites/Aptitudes/huipat.ts
+++ src/scenes/Entites/Aptitudes/huipat.ts
@@ -1,8 +1,13 @@
 import Entite from "../Entite"
+import ToileHuipatPrefab from "../Projectiles/ToileHuipatPrefab"
 import { deplacementBas, deplacementDroite, deplacementGauche, saut, stopEntite } from "./Defaut/deplacements"
-export function __InitialisationSpecifique(entite: Entite, aptitudes: any) {}
+export function __InitialisationSpecifique(entite: Entite, aptitudes: any) {
+  (entite as any).scene.groupe_projectile_toiles = entite.scene.physics.add.group({
+    runChildUpdate: true,
+    maxSize: 4
+  })
+}
 
-export function __toucheEspace(entite: Entite, aptitudes: any) { }
 
 export function __toucheDroite(entite: Entite, aptitudes: any) {
   deplacementDroite(entite)
@@ -19,6 +24,18 @@ export function __toucheHaut(entite: Entite, aptitudes: any) {
 export function __toucheBas(entite: Entite, aptitudes: any) {
   deplacementBas(entite)
 }
+
+export function __toucheEspace(entite: Entite) {
+  if (!entite.body.moves) return;
+  const toile = new ToileHuipatPrefab(entite.scene, entite.image_entite.getBounds().centerX, entite.image_entite.getBounds().centerY);
+  (entite as any).scene.groupe_projectile_toiles.add(toile);
+  toile.body.setVelocity(entite.image_entite.flipX ? -1300 : 1300, -200);
+
+  entite.scene.time.delayedCall(500, () => {
+    toile.destroy(true)
+  }, undefined, entite.scene);
+}
+
 
 export function __aucuneTouche(entite: Entite, aptitudes: any) {
   stopEntite(entite)
